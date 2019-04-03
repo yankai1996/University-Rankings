5ae8489ef8bf94529b5ca933baac28d7cad8a6be
Kai Yan <u3514123@connect.hku.hk>
2019-04-03 10:19:28 +0800

    update documentation

--- match.py
+++ match.py
@@ -1,4 +1,5 @@
-# Match universities names from two lists
+# 
+# Match university names from two lists
 # 
 
 from sas7bdat import SAS7BDAT
@@ -9,11 +10,11 @@ from progressbar import progressbar
 
 FILE_1 = 'qs2019.sas7bdat'
 FILE_2 = 'arwu2018.sas7bdat'
-col1 = 2
-col2 = 1
-id1 = -1
-id2 = -1
-outfile = 'qs_arwu.csv'
+COL_1 = 2	# column of the university names
+COL_2 = 1
+ID_1 = -1	# column of the university ids
+ID_2 = -1
+FILE_OUT = 'qs_arwu.csv'		# output file name
 
 
 # get two name lists
@@ -27,34 +28,35 @@ with SAS7BDAT(FILE_2) as f:
 	data2 = [row for row in f][1:]
 
 
+# Drop the useless part in the names
 def preprocess(name1, name2):
 	name1 = re.sub(r'\(.*?\)', '', name1)
 	name2 = re.sub(r'\(.*?\)', '', name2)
 	return name1, name2
 
 
-# match tow lists
-# fn is the matching mathod
-# base is to generalize the similarity
+# Match tow lists
+# fn: the matching mathod
+# preprocess: preprocess function
+# base: denominator to generalize the similarity
 def match_by(fn, preprocess=None, base=1):
 	# rows = []
-	with open(outfile, 'w', newline='') as csvfile:
+	with open(FILE_OUT, 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile)
-		writer.writerow(['name1', 'id1', 'name2', 'id2', 'similarity'])
+		writer.writerow(['name1', 'ID_1', 'name2', 'ID_2', 'similarity'])
 		for obs1 in progressbar(data1):
-			name1 = obs1[col1]
+			name1 = obs1[COL_1]
 			score = 0
 			# print(name1, name2, score)
 			for obs2 in data2:
-				name2 = obs2[col2]
-				if preprocess:
-					name1, name2 = preprocess(name1, name2)
+				name2 = obs2[COL_2]
+				name1, name2 = preprocess(name1, name2) if preprocess else name1, name2
 				new_score = fn(name1, name2)
 				if new_score > score:
 					score = new_score
 					match_name = name2
-					match_id = obs2[id2]
-			row = [name1, obs1[id1], match_name, match_id, score/base]
+					match_id = obs2[ID_2]
+			row = [name1, obs1[ID_1], match_name, match_id, score/base]
 			# print(row)
 			# rows.append(row)
 			writer.writerow(row)
